89b2dec01bf373acc397bec7b2a771ff95a9f279
Marcos Nolasco <marcosabn@gmail.com>
2021-10-06 23:22:31 -0300

    model

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>
-    <h1 v-for="number in numbers" :key="number.id">{{ number }}</h1>
-  </div>
+  <form>
+    <input type="text" placeholder="Nome" v-model.lazy.trim="name" />
+
+    <input type="number" placeholder="Idade" v-model.number="age" />
+
+    <textarea rows="2" placeholder="Descrição" v-model="message"></textarea>
+
+    <input type="checkbox" v-model="newsletter" /> Newsletter <br />
+
+    <input type="radio" name="color" value="vermelho" v-model="color" />
+    Vermelho
+    <input type="radio" name="color" value="verde" v-model="color" /> Verde
+    <input type="radio" name="color" value="azul" v-model="color" /> Azul
+
+    <select v-model="fruit">
+      <option disabled selected>Selecione uma fruta</option>
+      <option value="uva">Uva</option>
+      <option value="banana">Banna</option>
+      <option value="morango">Morango</option>
+    </select>
+  </form>
+
+  <p>Nome: {{ name }}</p>
+  <p>Idade: {{ age }}</p>
+  <p>Mensagem: {{ message }}</p>
+  <p>Newsletter: {{ newsletter }}</p>
+  <p>Cor: {{ color }}</p>
+  <p>Fruta: {{ fruit }}</p>
 </template>
 
 <script lang="ts">
@@ -10,27 +35,27 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   data() {
     return {
-      numbers: [1, 2, 3, 4, 5, 6],
+      name: '',
+      age: 0,
+      message: '',
+      newsletter: false,
+      color: '',
+      fruit: '',
     }
   },
 })
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+input[type='email'],
+input[type='text'],
+select,
+textarea {
+  display: block;
 }
 
-.danger {
-  color: red;
-}
-
-.success {
-  color: green;
+form {
+  padding-bottom: 1rem;
+  border-bottom: 2px solid #000;
 }
 </style>
