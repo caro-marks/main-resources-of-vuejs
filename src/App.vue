60be2e5c4993e2920af623887027ad17a46f236f
Marcos Nolasco <marcosabn@gmail.com>
2021-10-07 18:46:05 -0300

    sintaxe de objeto

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,7 @@
 <template>
-  <form>
-    <h1 v-railai:background.shadow="'purple'">FORMULARIO CARAIO</h1>
-    <input type="text" placeholder="Nome" v-model.lazy.trim="name" />
-
-    <input type="number" placeholder="Idade" v-model.number="age" />
-
-    <textarea rows="2" placeholder="Descrição" v-model="message"></textarea>
-
-    <input type="checkbox" v-model="newsletter" /> Newsletter <br />
-
-    <input type="radio" name="color" value="vermelho" v-model="color" />
-    Vermelho
-    <input type="radio" name="color" value="verde" v-model="color" /> Verde
-    <input type="radio" name="color" value="azul" v-model="color" /> Azul
-
-    <select v-model="fruit">
-      <option disabled selected>Selecione uma fruta</option>
-      <option value="uva">Uva</option>
-      <option value="banana">Banna</option>
-      <option value="morango">Morango</option>
-    </select>
-  </form>
-
-  <p>Nome: {{ name }}</p>
-  <p>Idade: {{ age }}</p>
-  <p>Mensagem: {{ message }}</p>
-  <p>Newsletter: {{ newsletter }}</p>
-  <p>Cor: {{ color }}</p>
-  <p>Fruta: {{ fruit }}</p>
+  <h1 class="static" :class="{ active: isActive, 'text-danger': hasError }">
+    FALASERIOVEI
+  </h1>
 </template>
 
 <script lang="ts">
@@ -36,27 +10,9 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   data() {
     return {
-      name: '',
-      age: 0,
-      message: '',
-      newsletter: false,
-      color: '',
-      fruit: '',
+      isActive: true,
+      hasError: false,
     }
   },
 })
 </script>
-
-<style scoped>
-input[type='email'],
-input[type='text'],
-select,
-textarea {
-  display: block;
-}
-
-form {
-  padding-bottom: 1rem;
-  border-bottom: 2px solid #000;
-}
-</style>
