90c2d4bc2afe25c590218ea1bac5c526dd5cd07f
Marcos Nolasco <marcosabn@gmail.com>
2021-10-08 12:06:09 -0300

    dynamic

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,38 @@
 <template>
-  <styled-button text="Cadastro" color="success" @clique="action($event)">
-    <template v-slot:before>
-      <i class="fas fa-hand-spock"></i>
-    </template>
-    <template v-slot:after>
-      <i class="far fa-hand-spock"></i>
-    </template>
-  </styled-button>
+  <div class="main">
+    <ul>
+      <li @click="currentView = 'Lar'">Home</li>
+      <li @click="currentView = 'Sobre'">Sobre</li>
+      <li @click="currentView = 'Contato'">Contato</li>
+    </ul>
+    <styled-button text="Cadastro" color="success" @clique="action($event)">
+      <template v-slot:before>
+        <i class="fas fa-hand-spock"></i>
+      </template>
+      <template v-slot:after>
+        <i class="far fa-hand-spock"></i>
+      </template>
+    </styled-button>
+    <component :is="currentView" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import Home from '@/pages/Home.vue'
+import About from '@/pages/About.vue'
+import Contact from '@/pages/Contact.vue'
+
 export default defineComponent({
+  components: {
+    Lar: Home,
+    Sobre: About,
+    Contato: Contact,
+  },
   data() {
     return {
-      user: {
-        id: 2,
-        name: 'Marcos',
-      },
+      currentView: 'Home',
     }
   },
   methods: {
