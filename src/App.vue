cc033aa7c710e4d1c7f5bf6b96e8c80d341f9308
Marcos Nolasco <marcosabn@gmail.com>
2021-10-08 11:56:10 -0300

    global

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
-  <ButtonStyled text="Cadastro" color="success" @clique="action($event)">
+  <styled-button text="Cadastro" color="success" @clique="action($event)">
     <template v-slot:before>
       <i class="fas fa-hand-spock"></i>
     </template>
     <template v-slot:after>
       <i class="far fa-hand-spock"></i>
     </template>
-  </ButtonStyled>
+  </styled-button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import StyledButton from '@/components/BotaoEstilizado.vue'
 
 export default defineComponent({
-  components: {
-    ButtonStyled: StyledButton,
-  },
   data() {
     return {
       user: {
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import highlight from '@/directives/highlight'
 import '@fortawesome/fontawesome-free/css/all.css'
+import StyledButton from '@/components/BotaoEstilizado.vue'
 
 const app = createApp(App)
 
 app.directive('railai', highlight)
+app.component('styled-button', StyledButton)
 
 app.mount('#app')
