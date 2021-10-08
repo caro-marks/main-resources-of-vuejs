83d0c3139b74b2bba365276a24c63f5e1486eb90
Marcos Nolasco <marcosabn@gmail.com>
2021-10-08 16:12:28 -0300

    mode

--- src/App.vue
+++ src/App.vue
@@ -1,131 +1,37 @@
 <template>
-  <div class="main">
-    <ul>
-      <li @click="currentView = 'Lar'">Home</li>
-      <li @click="currentView = 'Sobre'">Sobre</li>
-      <li @click="currentView = 'Contato'">Contato</li>
-    </ul>
+  <ul>
+    <li @mouseover="view = 'Lar'">home</li>
+    <li @click="view = 'Sobre'">about</li>
+    <li @click="view = 'Contato'">contato</li>
+  </ul>
 
-    <styled-button text="Cadastro" color="success" @clique="action($event)">
-      <template v-slot:before>
-        <i class="fas fa-hand-spock"></i>
-      </template>
-      <template v-slot:after>
-        <i class="far fa-hand-spock"></i>
-      </template>
-    </styled-button>
-
-    <button @click="show = !show">Toogle</button>
-
-    <transition
-      enter-active-class="animate__animated animate__slideInLeft"
-      leave-active-class="animate__animated animate__slideOutRight"
-      appear
-    >
-      <component :is="currentView" v-if="show" />
-    </transition>
-    <!-- <transition
-      @before-enter="beforeEnter"
-      @enter="enter"
-      @after-enter="afterEnter"
-      @enter-cancelled="enterCancelled"
-      @before-leave="beforeLeave"
-      @leave="leave"
-      @after-leave="afterLeave"
-      @leave-cancelled="leaveCancelled"
-      :css="false"
-    >
-    </transition> -->
-  </div>
+  <transition
+    enter-active-class="animate__animated animate__bounceIn"
+    leave-active-class="animate__animated animate__bounceOut"
+    :appear="true"
+    mode="out-in"
+  >
+    <component :is="view" />
+  </transition>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-
-import Home from '@/pages/Home.vue'
-import About from '@/pages/About.vue'
-import Contact from '@/pages/Contact.vue'
-
 import 'animate.css'
+import HOME from '@/pages/Home.vue'
+import CONTACT from '@/pages/Contact.vue'
+import ABOUT from '@/pages/About.vue'
 
 export default defineComponent({
   components: {
-    Lar: Home,
-    Sobre: About,
-    Contato: Contact,
+    Lar: HOME,
+    Contato: CONTACT,
+    Sobre: ABOUT,
   },
   data() {
     return {
-      currentView: 'Home',
-      show: true,
+      view: 'Lar',
     }
-  },
-  methods: {
-    action(acao: string) {
-      alert(acao)
-    },
-    // --------
-    // ENTERING
-    // --------
-    beforeEnter(el: HTMLElement) {
-      // ...
-      el.classList.add('test')
-    },
-    /* 
-    enter(el, done) {
-      // ...
-      done()
-    },
-    enterCancelled(el) {
-      // ...
-    }, 
-    afterEnter(el: HTMLElement) {
-      // ...
-      el.classList.add('test')
-    },*/
-
-    // --------
-    // LEAVING
-    // --------
-    /*  beforeLeave(el) {
-      // ...
-    },
-    leave(el, done) {
-      // ...
-      done()
-    },
-    afterLeave(el) {
-      // ...
-    },
-    // disponível apenas com v-show
-    leaveCancelled(el) {
-      // ...
-    }, */
   },
 })
 </script>
-
-<style scoped>
-.test {
-  background: cadetblue;
-}
-/* 
-v-enter-from (de onde estar vindo)
-v-enter-active (como vai acontecer)
-v-enter-to (para onde está indo)
-
-v-leave-from (de onde estar vindo)
-v-leave-active (como vai acontecer)
-v-leave-to (para onde está indo)
-
-O nome inicial `v` depende do atributo `name`
-*/
-/* .fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-.daonde,
-.praonde {
-  transition: opacity 0.5s;
-} */
-</style>
